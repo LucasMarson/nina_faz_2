ee40230d1fa22791c3d51087859c388bc5fea80e
Lucas Mateus Marson <lucas-marson@hotmail.com>
2023-01-24 11:57:43 -0300

    Add files via upload

---
+++ src/components/Days/index.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react";
+
+type DaysProps = {
+    title: string
+    description?: string
+    children: ReactNode
+}
+
+export function Days({title, description, children}: DaysProps) {
+  return (
+    <div className="flex flex-col">
+      <div className="flex gap-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 50 50"
+          className="h-[50px]"
+          fill="#fff"
+        >
+          {children}
+        </svg>
+        <span className="text-5xl mb-6 text-white font-normal">{title}</span>
+      </div>
+      <span className="text-[#08336d] text-xl">
+        {description}
+      </span>
+    </div>
+  );
+}
